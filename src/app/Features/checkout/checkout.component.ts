e045c5586e4577c8fe3eaa7bfeb3be94cff4e22b
Ibrahim Elsayed <ibrahimelsayedali2002@gmail.com>
2026-08-14 19:01:51 +0300

    implemen checkout

---
+++ src/app/Features/checkout/checkout.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  imports: [],
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CheckoutComponent {}
